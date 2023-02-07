e65cf0ff49212b4547dfd249e12a607d9d1fdca3
tobiastymkiw <tobiastymkiw@gmail.com>
2023-02-06 23:05:49 -0300

    These are just commits, I swear Im going to do something!

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 export default function App(): JSX.Element {
   return (
     <Fragment>
-      <h1>Hello World!!</h1>;
+      <h1>Hello World!!!</h1>;
     </Fragment>
   );
 }
